b99aa634aa92b1cc68f0c0bd587674e47b10d953
tam nguyen <ndtam.273@gmail.com>
2020-01-07 06:02:57 +0700

    change files

--- src/app.js
+++ src/app.js
@@ -1,46 +1,95 @@
-console.log('App.js is running!');
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing four'];
 
-
-var templateObj = {
-   title: 'Indecision App',
-   subTitle: 'Put your life in a hand of a computer',
-   options: ['One', 'Two']
-};
-
-// JSX - Javascript XML
-var template = (
-    <div>
-        <h1>{templateObj.title}</h1>
-        {templateObj.subTitle && <p>{templateObj.subTitle}</p>}
-        <p>{templateObj.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
-
-var user = {
-   name : 'Tam',
-   age : 30,
-   location : 'Ho Chi Minh'
-};
-function getLocation(location) {
-  if (location) {
-      return <p>Location: {location}</p>;
-  } 
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    );
+  }
 }
-// Create a template JSX expression
-var template2 = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        <p>This is my first challenge</p>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-        
-    </div>
-);
-var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
+  }
+}
 
+class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handlePick}>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll() {
+    console.log(this.props.options);
+    // alert('handleRemoveAll');
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.optionText}</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
