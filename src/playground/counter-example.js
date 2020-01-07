9f8e509c7c85fc4911cc62bebaaf942a9332ea02
tam nguyen <ndtam.273@gmail.com>
2020-01-08 06:05:35 +0700

    counter props default

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,9 +5,11 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: 0
+      count: props.count
     };
   }
+
+ 
   handleAddOne() {
     this.setState(
       (prevState) => {
@@ -42,5 +44,9 @@ class Counter extends React.Component {
     );
   }
 }
+
+Counter.defaultProps = {
+  count: 0
+};
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
