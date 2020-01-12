7372da3d2f8ce2d303cd45d1058d5d8dc6d47c73
tam nguyen <ndtam.273@gmail.com>
2020-01-12 20:51:28 +0700

    add sass loader, create folder for scss style

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/Indecision';
-
+import './styles/styles.scss';
 
   ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
   
